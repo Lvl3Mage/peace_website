8bb17f93932f95b3690efba3fc35157870620080
Lvl3Mage <lvlthreemage@gmail.com>
2021-10-21 09:02:04 +0200

    Made the current content of the page responsive

--- peace_site/public_html/assets/js/custom.js
+++ peace_site/public_html/assets/js/custom.js
@@ -30,19 +30,31 @@ $(document).ready( function () {
 		slidesToScroll: 1,
 		responsive: [
 			{
-				breakpoint: 1024,
+				breakpoint: 1200,
 				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
+					slidesToShow: 5,
+					slidesToScroll: 1,
 				}
 			},{
-				breakpoint: 600,
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 2
+				}
+			},{
+				breakpoint: 640,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},{
+				breakpoint: 576,
 				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 2
 				}
 			},{
-				breakpoint: 480,
+				breakpoint: 420,
 				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1
